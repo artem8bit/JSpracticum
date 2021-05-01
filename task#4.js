713d03f93a85918d033249c19086b36e47f32b4a
mentos_i_cola <bodunov232@gmail.com>
2021-05-01 20:24:55 +0300

    task_4

--- task#4.js
+++ task#4.js
@@ -1,1 +1,9 @@
-//в процессе
+const accum = str => str
+  .split('')
+  .map((ch, i) => (ch = ch.toLowerCase().repeat(i+1)) && ch.charAt().toUpperCase() + ch.slice())
+  .join('-');
+
+console.log(accum("abcd"));    // "A-Bb-Ccc-Dddd"
+console.log(accum("RqaEzty")); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+console.log(accum("cwAt"));    // "C-Ww-Aaa-Tttt
+
