e93f4b003df2e8ff6fd6aa635939484afd9adee5
mentos_i_cola <bodunov232@gmail.com>
2021-05-01 20:54:12 +0300

    task_4

--- task#4.js
+++ task#4.js
@@ -1,9 +1,8 @@
 const accum = str => str
   .split('')
-  .map((ch, i) => (ch = ch.toLowerCase().repeat(i+1)) && ch.charAt().toUpperCase() + ch.slice())
+  .map((ch, i) => (ch = ch.toLowerCase().repeat(i + 1)) && ch.charAt(0).toUpperCase() + ch.slice(1))
   .join('-');
 
 console.log(accum("abcd"));    // "A-Bb-Ccc-Dddd"
 console.log(accum("RqaEzty")); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
-console.log(accum("cwAt"));    // "C-Ww-Aaa-Tttt
-
+console.log(accum("cwAt"));    // "C-Ww-Aaa-Tttt"
